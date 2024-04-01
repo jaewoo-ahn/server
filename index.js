501b557e90c547bdfd22e8a8d1b423b20e2ca904
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-04-01 20:22:15 +0900

    router

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import { Configuration, OpenAIApi } from "openai";
+import openaiRoutes from "./routes/openai.js";
 
 // configuration
 dotenv.config();
@@ -26,6 +27,9 @@ const configuration = new Configuration({
 });
 
 export const openai = new OpenAIApi(configuration);
+
+//routes
+app.use("/openai", openaiRoutes);
 
 // server setup
 const PORT = process.env.PORT || 9000;
