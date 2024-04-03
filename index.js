d9f227536cabdb898b7f92a20b9486ba78e20007
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-04-03 22:48:13 +0900

    error

--- index.js
+++ index.js
@@ -4,35 +4,32 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-import { Configuration, OpenAIApi } from "openai";
-import openaiRoutes from "./routes/openai.js";
+import OpenAIApi from "openai";
+import Configuration from "openai";
+import openAiRoutes from "./routes/openai.js";
 
-// configuration
+/* CONFIGURATIONS */
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-//openai configuration
-const { Configuration, OpenAIApi } = require("openai");
-
+/* OPEN AI CONFIGURATION */
 const configuration = new Configuration({
   apiKey: process.env.OPEN_API_KEY,
 });
-
 export const openai = new OpenAIApi(configuration);
 
-//routes
-app.use("/openai", openaiRoutes);
+/* ROUTES */
+app.use("/openai", openAiRoutes);
 
-// server setup
+/* SERVER SETUP */
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
-  console.log(`example app listening at http://localhost:${PORT}`);
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
