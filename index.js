e2fcf3fa4b2348a5642c5c1645c8f19916b963ce
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-04-04 19:10:37 +0900

    chat server

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-import OpenAIApi from "openai";
 import Configuration from "openai";
+import OpenAIApi from "openai";
 import openAiRoutes from "./routes/openai.js";
+import authRoutes from "./routes/auth.js";
 
-/* CONFIGURATIONS */
+/* configuration */
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -19,16 +20,17 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-/* OPEN AI CONFIGURATION */
+/* open ai configuration */
 const configuration = new Configuration({
   apiKey: process.env.OPEN_API_KEY,
 });
 export const openai = new OpenAIApi(configuration);
 
-/* ROUTES */
+/* routes */
 app.use("/openai", openAiRoutes);
+app.use("/auth", authRoutes);
 
-/* SERVER SETUP */
+/* server setup */
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
